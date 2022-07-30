039a670bd82c7813a749886624e58553a4745e74
insub4067 <vb15@kakao.com>
2022-07-30 23:36:29 +0900

    [Add File] DetailPage

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MainPage from './Pages/MainPage'
 import AboutPage from './Pages/AboutPage'
+import DetailPage from './Pages/DetailPage';
 
 export default function App() {
   return (
-    <AboutPage/>
+    <DetailPage/>
   );
 }
 
